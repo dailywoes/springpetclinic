f3e5d91cdb4b2c4d76e118e1061104e1004f2bb0
John Tex <johnrteixeira@gmail.com>
2022-01-03 22:48:07 -0500

    added service map

--- springpetclinic-web/src/main/java/com/johnrteixeira/springpetclinic/controllers/OwnerController.java
+++ springpetclinic-web/src/main/java/com/johnrteixeira/springpetclinic/controllers/OwnerController.java
@@ -1,13 +1,24 @@
 package com.johnrteixeira.springpetclinic.controllers;
 
+import com.johnrteixeira.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerservice;
+
+    public OwnerController(OwnerService ownerservice) {
+        this.ownerservice = ownerservice;
+    }
+
     @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerservice.findAll());
+
         return "owners/index";
     }
 }
