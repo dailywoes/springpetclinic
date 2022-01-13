c3aea62105729b084d2a0c5b20234385bf26c717
John Tex <johnrteixeira@gmail.com>
2022-01-13 11:55:36 -0500

    Additng JPA entities

--- springpetclinic-web/src/main/java/com/johnrteixeira/springpetclinic/controllers/OwnerController.java
+++ springpetclinic-web/src/main/java/com/johnrteixeira/springpetclinic/controllers/OwnerController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
@@ -14,11 +15,16 @@ public class OwnerController {
         this.ownerservice = ownerservice;
     }
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model) {
 
         model.addAttribute("owners", ownerservice.findAll());
 
         return "owners/index";
+    }
+
+    @RequestMapping({"/find"})
+    public String findOwners(){
+        return "notimplemented";
     }
 }
